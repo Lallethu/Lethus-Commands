7af4aaf86714f03b87f76a57a64e5b3bbb7043b1
= <Lallethu.68@gmail.com>
2023-07-11 20:00:52 +0200

    Import both my previous classes and modify the register setup to register my items and their events

--- src/main/java/net/lallethu/lethuscommands/LethusCommands.java
+++ src/main/java/net/lallethu/lethuscommands/LethusCommands.java
@@ -1,10 +1,13 @@
 package net.lallethu.lethuscommands;
 
 import com.mojang.logging.LogUtils;
+import net.lallethu.lethuscommands.blocks.ModBlocks;
+import net.lallethu.lethuscommands.items.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -25,8 +28,11 @@ public class LethusCommands {
 	private static final Logger LOGGER = LogUtils.getLogger();
 
 	public LethusCommands() {
-		// Register the setup method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		// Register items and block from mod by using the eventBus interface
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModItems.register(eventBus);
+		ModBlocks.register(eventBus);
+		eventBus.addListener(this::setup);
 
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
